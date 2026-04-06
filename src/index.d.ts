ad30a3cb610bb7775fd025d7e45a7d6b30812bfd
Ethan Miller <ethan.w.miller@outlook.com>
2026-04-05 20:59:16 -0700

    update .d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,7 @@ export function createStore<T extends Record<string, unknown>>(
 ): {
   get(): T;
   get<K extends keyof T>(key: K): T[K];
+  getSnapshot(): T;
   set<K extends keyof T>(key: K, value: T[K]): void;
   reset(resetState?: T): void;
   subscribe(listener: (state: T) => void): () => void;
